9b473783cc4d0bc141dc4081fe1e8356617b4c06
Velia Precious <veliaprecious@gmail.com>
2024-05-10 20:11:35 +0100

    navbar update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import useMediaQuery from "@/Hooks/useMediaQuery";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const isAboveMediumScreens = useMediaQuery("(min-width: 700px)");
+
+  return (
+    <nav className="flex w-full justify-between py-2 px-4">
+      <div>Carefinder</div>
+
+      {isAboveMediumScreens ? (
+        <div className="flex justify-between gap-3">
+          <div className="flex justify-between gap-3">
+            <Link>Home</Link>
+            <Link>About</Link>
+            <Link>Find Hospital</Link>
+          </div>
+
+          <div className="flex justify-between gap-3">
+            <button className="bg-blue-700 py-2 px-4 rounded">Login</button>
+            <button>Signup</button>
+          </div>
+        </div>
+      ) : (
+        <div>bars</div>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
