4ab946985f073715aef7e0bfba871dbaca11d225
Velia Precious <veliaprecious@gmail.com>
2024-05-10 09:39:08 +0100

    added usemediaquery hook

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,27 +1,28 @@
 import React from "react";
+import useMediaQuery from "../Hooks/useMediaQuery";
 
 const Home = () => {
+  // const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const isAboveMediumScreens = useMediaQuery("(min-width: 700px)");
   return (
     <div>
-      Home
-      <p className="text-3xl font-bold underline text-red-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-        architecto vel dicta, expedita, veniam quae nobis ab accusantium quaerat
-        repellat eaque eius natus quas reiciendis fugiat? Quod assumenda
-        doloremque beatae voluptas magnam sequi, fugiat quibusdam ipsum non
-        commodi asperiores placeat vero obcaecati! Dolorem neque, nostrum
-        similique officia nemo odit quibusdam porro quae sit maxime, ex sapiente
-        vero sed deleniti? Dignissimos voluptatem necessitatibus reprehenderit
-        nobis sed, nihil nemo nam? Ea dolore officiis, labore ipsum possimus
-        voluptatibus molestias maxime nihil, minus, et eius sequi velit facere
-        sit optio illo architecto eveniet tenetur modi laudantium aperiam
-        excepturi. Quaerat enim temporibus illum, facilis doloribus minus ullam
-        recusandae nemo itaque hic! Doloremque laborum facere reiciendis labore
-        perspiciatis ea, aperiam vel voluptas illo dicta laboriosam excepturi
-        modi, sapiente iusto corrupti repellat ad quisquam at itaque nisi
-        temporibus autem sed provident veniam. Quia a reiciendis eos eaque
-        expedita enim praesentium et. Tenetur ea iure aperiam nostrum deserunt.
-      </p>
+      {isAboveMediumScreens ? (
+        <div className="text-3xl font-bold underline text-red-400">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. A id,
+          sapiente molestias commodi incidunt ut. Pariatur qui totam sed
+          deserunt. Beatae animi iusto nisi minima laudantium ratione pariatur
+          dicta? Quaerat iure voluptas incidunt fuga odit eius ipsa. Dolorem,
+          quam ad?
+        </div>
+      ) : (
+        <div className="text-3xl font-bold underline text-purple-400">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa
+          veritatis placeat animi aspernatur error reprehenderit aliquam
+          expedita, libero neque quos eaque illo ut nisi quia repellendus nobis
+          tempore quae accusantium iure blanditiis odio. Earum dolore distinctio
+          totam! Officiis, ratione ipsum.
+        </div>
+      )}
     </div>
   );
 };
