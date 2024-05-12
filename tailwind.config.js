1ed422a255420b3ab5aad7e05965ddf60db9924b
marthamaxwell <marthamaxwell499@gmail.com>
2024-05-12 14:30:17 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: "#08299B",
+        softBlue: "#BFCAE0",
+        softBlueI: "#8193B8",
+        softBlueII: "#697A9B",
+        footerCol: "#E0E4EC",
+      },
+    },
   },
   plugins: [],
 };
