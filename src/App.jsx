c99084c14f5bb29d12887c651212ae5b4bd261e5
marthamaxwell <marthamaxwell499@gmail.com>
2024-06-25 22:41:18 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import MainLayout from "./Layouts/MainLayout";
 import mainRoutes from "./Routes/mainRoutes";
 import NotFoundPage from "./Pages/NotFoundPage";
 
+import MapPage from "./Pages/mapPage";
+import ContactPg from "./Pages/ContactPg";
+import MediaPage from "./Pages/MediaPage";
+import LoginPage from "./Pages/loginPage";
+
 function App() {
   const router = createBrowserRouter([
     {
@@ -11,6 +16,22 @@ function App() {
       element: <MainLayout />,
       children: mainRoutes,
       errorElement: <NotFoundPage />,
+    },
+    {
+      path: "/map",
+      element: <MapPage />,
+    },
+    {
+      path: "/contact",
+      element: <ContactPg />,
+    },
+    {
+      path: "/media",
+      element: <MediaPage />,
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
     },
   ]);
   return <RouterProvider router={router} />;
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,33 +1,57 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const phoneNumber = "+2349112989342";
+
+  const redirectToWhatsapp = () => {
+    const url = "https://wa.me/message/T36TTGQGDXIHC1";
+    window.open(url, "_blank");
+  };
+  // const handleDialerClick = () => {
+  //   const telUrl = `tel:${phoneNumber}`;
+  //   window.open(telUrl, '_self');
+  // };
   return (
     <div className=" flex justify-between  bg-footerCol py-10 px-16 flex-wrap">
       {/* CareFinder */}
-      <div className="mr-5">
-        <h4 className="mb-8 text-darkBlue font-bold tracking-wider">
+      <div className="mr-5 text-base">
+        <h4 className="sm:mb-8 mb-3 text-darkBlue font-bold tracking-wider">
           CareFinder
         </h4>
-        <p className="pb-1">Plot 42, Akinza Street,</p>
-        <p className="pb-1">Victoria island, Lagos</p>
-        <p>+2349167351788</p>
+        <Link to="/map">
+          <p className="pb-1">Plot 42, Akinza Street,</p>
+
+          <p className="pb-1">Victoria island, Lagos</p>
+        </Link>
+        <p onClick={redirectToWhatsapp} className="cursor-pointer">
+          +2349112989342
+        </p>
       </div>
       {/* About us */}
-      <div>
-        <h4 className="mb-8 text-darkBlue font-bold tracking-wider">
+      <div className="sm:mt-0 mt-5">
+        <h4 className="sm:mb-8 mb-3 text-darkBlue font-bold tracking-wider ">
           About Us
         </h4>
-        <p className="pb-1">News & Media</p>
-        <p className="pb-1">Contact Us</p>
+        <Link to="/media">
+          <p className="pb-1">News & Media</p>
+        </Link>
+        <Link to="/contact">
+          <p className="pb-1">Contact Us</p>
+        </Link>
       </div>
       {/* Quick links */}
-      <div>
-        <h4 className="mb-8 text-darkBlue font-bold tracking-wider">
+      <div className="sm:mt-0 mt-5">
+        <h4 className="sm:mb-8 mb-3 text-darkBlue font-bold  tracking-wider">
           Quick Links
         </h4>
-        <p className="pb-1">My account</p>
-        <p className="pb-1">Book an appointment</p>
-        <p>Library</p>
+        <Link to="/login">
+          <p className="pb-1">My account</p>
+        </Link>
+        <Link to="/login">
+          <p className="pb-1">Book an appointment</p>
+          <p>Library</p>{" "}
+        </Link>
       </div>
     </div>
   );
